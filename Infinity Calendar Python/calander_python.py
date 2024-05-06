c09a499b6b954708e6090a35722a8bd5dd7d0e94
Jetur Gavli <jeturgavli1in1@gmail.com>
2024-05-06 15:37:48 +0530

    Infinty Calendar Using Python #1

---
+++ Infinity Calendar Python/calander_python.py
@@ -0,0 +1,5 @@
+import calendar
+print("JETRock infinty Calendar")
+year = int(input("Enter year of calendar : "))
+month = int(input("Enter month of calendar : "))
+print (calendar.month(year,month))
